b358adfc802e1a860fab3bb67c374648bc349172
Steveen Vargas <csv17@live.unc.edu>
2019-01-31 18:04:52 +0000

    Second commit

--- main.hs
+++ main.hs
@@ -4,4 +4,4 @@ finalGrade g w
  | length w == 0 = 0
  | ((length g) && (length w)) > 1 = ([g*w | g<- [1..10], w[1..10]) -- tried to create a union of two integers between the weights and the grades so they can be later devided
  | ((length g) && (length w)) > 1 = ((length g) * (length w) `div` (length w) -- tried to get the weighted average of the grades
- | otherwise = x y
+ | otherwise = 0
